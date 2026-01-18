af841a586f7ed26d3da6dc6d95935a34382ee4ab
krasov <me@frostfree.ru>
2026-01-18 19:33:09 +0300

    add tracing

--- apps/api-gateway/app/main.py
+++ apps/api-gateway/app/main.py
@@ -1,17 +1,20 @@
 from contextlib import asynccontextmanager
+from typing import AsyncGenerator
 from prometheus_client import make_asgi_app
 from fastapi import FastAPI
 import uvicorn
 
 from config import logger, LOGGING_CONFIG
-from metrics import REQUESTS_TOTAL, REQUEST_LATENCY, IN_PROGRESS_REQUESTS
-
+from tracing import process_endpoint
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
 
 @asynccontextmanager
-async def lifespan(app: FastAPI):
+async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
+    logger.info("Mounting /metrics endpoint...")
     metrics_app = make_asgi_app()
     app.mount("/metrics", metrics_app)
-    logger.info("Mounting /metrics endpoint...")
+    logger.info("Setting up tracing...")
+    FastAPIInstrumentor.instrument_app(app)
     yield
     logger.info("Application shutdown...")
 
@@ -21,14 +24,14 @@ app = FastAPI(lifespan=lifespan)
 
 @app.get("/")
 async def root():
-    operation = "root"
-    REQUESTS_TOTAL.labels(operation=operation).inc()
-    with REQUEST_LATENCY.labels(operation=operation).time():
-        IN_PROGRESS_REQUESTS.labels(operation=operation).inc()
-        try:
-            return {"message": "Hello, World!"}
-        finally:
-            IN_PROGRESS_REQUESTS.labels(operation=operation).dec()
+    with process_endpoint(path="/", method="GET"):
+        return {"message": "Hello, World!"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int) -> dict:
+    with process_endpoint(path="/items/:itemId", method="GET"):
+        return {"item_id": item_id}
 
 
 if __name__ == "__main__":
